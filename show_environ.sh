0d66a6d45d46eaf1bb8cda92e168c16658f5ad28
Sebb <sebb@apache.org>
2024-05-30 13:18:07 +0100

    4.7.0 quiet

--- show_environ.sh
+++ show_environ.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -x
-export
-ls -l
-pip list
-python3 -m pelican --version
-which -a pelican
-python3 -V
+# set -x
+# export
+# ls -l
+# pip list
+# python3 -m pelican --version
+# which -a pelican
+# python3 -V
